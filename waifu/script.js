b150c31c4c2858e436ca290a3af879de1e5e4afb
Phuc <hoangphuc.25071995@gmail.com>
2024-10-30 02:27:29 +0700

    qwe

--- waifu/script.js
+++ waifu/script.js
@@ -1,14 +1,14 @@
 var buildUrl = "Build";
-var loaderUrl = buildUrl + "/development.loader.js?638657592854484369";
+var loaderUrl = buildUrl + "/development.loader.js?638658514980080641";
 var config = {
-    dataUrl: buildUrl + "/b201ed242801b9d9159b4ecdf7c34b75.data.unityweb",
-    frameworkUrl: buildUrl + "/c92f497f74a40370474a747852dfc40a.js.unityweb",
-    codeUrl: buildUrl + "/cc0a5f8efd063dff49668b02548daab8.wasm.unityweb",
-    symbolsUrl: buildUrl + "/d7bca6756c68d4fb9257f4e6c136c43a.json.unityweb",
+    dataUrl: buildUrl + "/07bf8d85af9118a9d34ea326c15e92da.data.unityweb",
+    frameworkUrl: buildUrl + "/c7710f24be4fd751b954c9ba3c7c3485.js.unityweb",
+    codeUrl: buildUrl + "/7184bf617602f486b09172becbfc714d.wasm.unityweb",
+    symbolsUrl: buildUrl + "/745fd54225dd98f7c9034b28da6fb8eb.json.unityweb",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "Mirailabs",
     productName: "Waifu Tap",
-    productVersion: "1.0.5",
+    productVersion: "1.0.4",
 
     cacheControl: function (url) {
         // Caching enabled for .data and .bundle files.
