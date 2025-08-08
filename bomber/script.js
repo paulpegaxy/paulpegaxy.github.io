0758bbb65a309f9cbfd46b04f785d8e3cf4d9e33
heavenhell8899 <heavenhell8899@gmail.com>
2025-08-09 01:57:46 +0700

    - init bomber

---
+++ bomber/script.js
@@ -0,0 +1,99 @@
+var buildUrl = "Build";
+var loaderUrl = buildUrl + "/development.loader.js?638903001189129665";
+var config = {
+    dataUrl: buildUrl + "/26c70891f79e81e5d07ee69305384f93.data.unityweb",
+    frameworkUrl: buildUrl + "/fd97be0983664d9514c5f8e9ac701984.js.unityweb",
+    codeUrl: buildUrl + "/0a2211b62f998f0e4f4867d2b27d9f92.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "bomb-idle",
+    productVersion: "0.1.10",
+
+    cacheControl: function (url) {
+        // Caching enabled for .data and .bundle files.
+        // Revalidate if file is up to date before loading from cache
+        if (url.match(/\.data/) || url.match(/\.bundle/)) {
+            return "must-revalidate";
+        }
+
+        // Caching enabled for .mp4 and .custom files
+        // Load file from cache without revalidation.
+        if (url.match(/\.mp4/) || url.match(/\.custom/)) {
+            return "immutable";
+        }
+
+        // Disable explicit caching for all other files.
+        // Note: the default browser cache may cache them anyway.
+        return "no-store";
+    },
+};
+
+var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+if (isMobile) {
+    // Define a maximum pixel ratio for mobile to avoid rendering at too high resolutions
+    const maxPixelRatioMobile = 2.0;
+    config.devicePixelRatio = Math.min(window.devicePixelRatio, maxPixelRatioMobile);
+}
+else {
+    config.devicePixelRatio = 2.0;
+}
+
+var canvas = document.querySelector("#unity-canvas");
+var loadingBar = document.querySelector("#unity-loading-fg");
+var textProgress = document.querySelector("#loading-progress-text");
+var unityGame;
+var script = document.createElement("script");
+script.src = loaderUrl;
+script.onload = function () {
+    createUnityInstance(canvas, config, function (progress) {
+        loadingBar.style.width = `${100 * progress}%`;
+        textProgress.innerHTML = `${Math.floor(progress * 100)}%`;
+    }).then(function (unityInstance) {
+        unityGame = unityInstance;
+    }).catch(function (message) {
+        alert(message);
+    });
+};
+document.body.appendChild(script);
+function runUnityCommand(method, params) {
+    unityGame?.SendMessage("WebBridge", method, params);
+}
+function UnityTaskCallBack(taskId, success, data) {
+    runUnityCommand("UnityTaskCallBack", JSON.stringify({
+        taskId,
+        success,
+        data: ((typeof data === 'object' && data !== null) ? JSON.stringify(data) : data.toString())
+    }));
+}
+
+
+const miraiWallet = {
+    appName: "miraiapp-tg",
+    name: "Mirai App",
+    imageUrl: "https://cdn.mirailabs.co/miraihub/miraiapp-tg-icon-288.png",
+    aboutUrl: "https://mirai.app",
+    universalLink: "https://t.me/mirai_app_dev_bot?attach=wallet",
+    bridgeUrl: "https://bridge.tonapi.io/bridge",
+    platforms: ["ios", "android", "macos", "windows", "linux"],
+}
+
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    manifestUrl: 'https://cdn.mirailabs.co/bomber/static/tonConnectConfig.json',
+    walletsListConfiguration: {
+        includeWallets: [miraiWallet]
+    }
+});
+
+tonConnectUI.onStatusChange(walletAndwalletInfo => {
+    runUnityCommand("StatusChange");
+});
+tonConnectUI.connectionRestored.then(restored => {
+    if (restored) {
+        console.log('Connection restored.');
+    } else {
+        console.log('Connection was not restored.');
+    }
+});
+
+
+
